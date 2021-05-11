44dff000632f8993c83677780de87f4d179cc487
Deonis Peretyagin <karudo@pushwoosh.com>
2021-05-11 10:01:38 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import MentionExample from './mentions';
+
+function App() {
+  return (
+    <div className="App">
+
+      <MentionExample />
+    </div>
+  )
+}
+
+export default App
